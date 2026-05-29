684e2833392d60e64b063b404b671280609a772c
Alan Guedes <a.guedes@reading.ac.uk>
2026-05-29 17:50:19 +0100

    fix main_av_test

--- test/main_av_test.dart
+++ test/main_av_test.dart
@@ -1,32 +1,69 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gingaf/main.dart';
 import 'package:gingaf/main_av.dart';
-import 'package:gingaf/html_app.dart';
-import 'package:ncl_app/ncl_app.dart';
+import 'package:video_player_platform_interface/video_player_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:video_player/video_player.dart';
 
-class MockAVAssetBundle extends CachingAssetBundle {
-  final Map<String, String> assets;
-
-  MockAVAssetBundle({required this.assets});
+class FakeVideoPlayerPlatform extends VideoPlayerPlatform
+    with MockPlatformInterfaceMixin {
+  final StreamController<VideoEvent> events = StreamController<VideoEvent>.broadcast();
 
   @override
-  Future<ByteData> load(String key) async {
-    return ByteData(0);
+  Future<void> init() async {}
+
+  @override
+  Future<void> dispose(int textureId) async {}
+
+  @override
+  Future<int?> create(DataSource dataSource) async {
+    return 1;
   }
 
   @override
-  Future<String> loadString(String key, {bool cache = true}) async {
-    if (assets.containsKey(key)) {
-      return assets[key]!;
-    }
-    throw FlutterError('MockAVAssetBundle: Unknown key $key');
+  Future<void> setLooping(int textureId, bool looping) async {}
+
+  @override
+  Future<void> play(int textureId) async {}
+
+  @override
+  Future<void> pause(int textureId) async {}
+
+  @override
+  Future<void> setVolume(int textureId, double volume) async {}
+
+  @override
+  Future<void> setPlaybackSpeed(int textureId, double speed) async {}
+
+  @override
+  Future<void> seekTo(int textureId, Duration position) async {}
+
+  @override
+  Future<Duration> getPosition(int textureId) async {
+    return Duration.zero;
+  }
+
+  @override
+  Stream<VideoEvent> videoEventsFor(int textureId) {
+    return events.stream;
+  }
+
+  @override
+  Widget buildView(int textureId) {
+    return Container();
   }
 }
 
 void main() {
-  testWidgets('MainAVController and MainAVWidget rendering', (WidgetTester tester) async {
+  late FakeVideoPlayerPlatform fakePlatform;
+
+  setUp(() {
+    fakePlatform = FakeVideoPlayerPlatform();
+    VideoPlayerPlatform.instance = fakePlatform;
+  });
+
+  testWidgets('MainAVController and MainAVWidget init, start, stop tests', (WidgetTester tester) async {
     final controller = MainAVController()..setMainAvUri('background.mp4');
 
     await tester.pumpWidget(
@@ -39,63 +76,25 @@ void main() {
 
     expect(find.text('Loading Background AV: background.mp4'), findsOneWidget);
 
-    controller.stop();
-    await tester.pump();
+    fakePlatform.events.add(VideoEvent(
+      eventType: VideoEventType.initialized,
+      duration: const Duration(seconds: 10),
+      size: const Size(100, 100),
+    ));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byType(VideoPlayer), findsOneWidget);
     expect(find.text('Loading Background AV: background.mp4'), findsNothing);
 
+    controller.stop();
+    await tester.pumpAndSettle();
+
+    expect(find.byType(VideoPlayer), findsNothing);
+
     controller.play();
-    await tester.pump();
-    expect(find.text('Loading Background AV: background.mp4'), findsOneWidget);
-  });
+    await tester.pumpAndSettle();
 
-  test('HTMLApp can change background video via callback', () {
-    String? changedUri;
-    final app = HTMLApp(
-      uri: 'test.html',
-      onBackgroundVideoChanged: (uri) {
-        changedUri = uri;
-      },
-    );
-
-    app.onBackgroundVideoChanged?.call('html_bg.mp4');
-    expect(changedUri, 'html_bg.mp4');
-  });
-
-  testWidgets('NCLApp can change background video via settings property', (WidgetTester tester) async {
-    String? changedUri;
-    const nclData = '''
-<ncl>
-  <body>
-    <port id="p1" component="m1"/>
-    <media id="m1" src="v1.mp4" type="video/mp4"/>
-    <settings id="s1">
-      <property name="videoUri" value="ncl_bg.mp4"/>
-    </settings>
-  </body>
-</ncl>
-''';
-
-    final mockBundle = MockAVAssetBundle(assets: {
-      'test_bg.ncl': nclData,
-    });
-
-    await tester.pumpWidget(
-      MaterialApp(
-        home: Scaffold(
-          body: DefaultAssetBundle(
-            bundle: mockBundle,
-            child: NCLApp(
-              uri: 'test_bg.ncl',
-              onBackgroundVideoChanged: (uri) {
-                changedUri = uri;
-              },
-            ),
-          ),
-        ),
-      ),
-    );
-    await tester.pump(const Duration(milliseconds: 500));
-
-    expect(changedUri, 'ncl_bg.mp4');
+    expect(find.byType(VideoPlayer), findsOneWidget);
   });
 }
