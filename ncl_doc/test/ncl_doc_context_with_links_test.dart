7b7d035855de120cecc18a38c12bf3194522de9a
Alan Guedes <a.guedes@reading.ac.uk>
2026-06-03 16:55:12 +0100

    more ctx tests

--- ncl_doc/test/ncl_doc_context_with_links_test.dart
+++ ncl_doc/test/ncl_doc_context_with_links_test.dart
@@ -129,5 +129,136 @@ void main() {
       expect(doc.getNodeById('m3')?.getMainState(), State.SLEEPING);
       expect(doc.getNodeById('m4')?.getMainState(), State.SLEEPING);
     });
+
+    test('3x ctx nested', () {
+      const xml = '''
+<ncl>
+  <body>
+    <port id="p1" component="ctx1"/>
+    <context id="ctx1">
+      <port id="p2" component="ctx2"/>
+      <context id="ctx2">
+        <port id="p3" component="ctx3"/>
+        <context id="ctx3">
+          <port id="p4" component="m1"/>
+          <media id="m1" src="v1.mp4"/>
+          <media id="m2" src="v2.mp4"/>
+          <link id="l1">
+            <bind role="onBegin" component="m1"/>
+            <bind role="start" component="m2"/>
+          </link>
+        </context>
+      </context>
+    </context>
+  </body>
+</ncl>
+''';
+      final doc = NCLDocument.fromXML(xml);
+      doc.start();
+      expect(doc.getBodyState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx1')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx2')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx3')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('m1')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('m2')?.getMainState(), State.OCCURRING);
+    });
+
+    test('4x ctx nested', () {
+      const xml = '''
+<ncl>
+  <body>
+    <port id="p1" component="ctx1"/>
+    <context id="ctx1">
+      <port id="p2" component="ctx2"/>
+      <context id="ctx2">
+        <port id="p3" component="ctx3"/>
+        <context id="ctx3">
+          <port id="p4" component="ctx4"/>
+          <context id="ctx4">
+            <port id="p5" component="m1"/>
+            <media id="m1" src="v1.mp4"/>
+            <media id="m2" src="v2.mp4"/>
+            <link id="l1">
+              <bind role="onBegin" component="m1"/>
+              <bind role="start" component="m2"/>
+            </link>
+          </context>
+        </context>
+      </context>
+    </context>
+  </body>
+</ncl>
+''';
+      final doc = NCLDocument.fromXML(xml);
+      doc.start();
+      expect(doc.getBodyState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx1')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx2')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx3')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx4')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('m1')?.getMainState(), State.OCCURRING);
+      expect(doc.getNodeById('m2')?.getMainState(), State.OCCURRING);
+    });
+
+    test('3x ctx nested no ports', () {
+      const xml = '''
+<ncl>
+  <body>
+    <context id="ctx1">
+      <context id="ctx2">
+        <context id="ctx3">
+          <media id="m1" src="v1.mp4"/>
+          <media id="m2" src="v2.mp4"/>
+          <link id="l1">
+            <bind role="onBegin" component="m1"/>
+            <bind role="start" component="m2"/>
+          </link>
+        </context>
+      </context>
+    </context>
+  </body>
+</ncl>
+''';
+      final doc = NCLDocument.fromXML(xml);
+      doc.start();
+      expect(doc.getBodyState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx1')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('ctx2')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('ctx3')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('m1')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('m2')?.getMainState(), State.SLEEPING);
+    });
+
+    test('4x ctx nested no ports', () {
+      const xml = '''
+<ncl>
+  <body>
+    <context id="ctx1">
+      <context id="ctx2">
+        <context id="ctx3">
+          <context id="ctx4">
+            <media id="m1" src="v1.mp4"/>
+            <media id="m2" src="v2.mp4"/>
+            <link id="l1">
+              <bind role="onBegin" component="m1"/>
+              <bind role="start" component="m2"/>
+            </link>
+          </context>
+        </context>
+      </context>
+    </context>
+  </body>
+</ncl>
+''';
+      final doc = NCLDocument.fromXML(xml);
+      doc.start();
+      expect(doc.getBodyState(), State.OCCURRING);
+      expect(doc.getNodeById('ctx1')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('ctx2')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('ctx3')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('ctx4')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('m1')?.getMainState(), State.SLEEPING);
+      expect(doc.getNodeById('m2')?.getMainState(), State.SLEEPING);
+    });
   });
 }
